be291bb0cf4518ee63154aefcfa242ace2cbfd15
Tuanle207 <letgo237@gmail.com>
2020-09-15 16:17:03 +0700

    Move App.js outside to app folder

--- app/App.js
+++ app/App.js
@@ -3,8 +3,8 @@ import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 
-import {store, persistor} from '../store'
-import Navigation from './Navigation';
+import {store, persistor} from './store'
+import Navigation from './components/Navigation';
 
 const App = () => {
     return (
