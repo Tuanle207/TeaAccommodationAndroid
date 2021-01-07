5c6a8d137a0f2f7059c38b5172e835b640bf6ff0
David Tuan <55757870+18521604@users.noreply.github.com>
2021-01-07 13:56:19 +0700

    Completed Update User Screen with validations

--- app/components/screens/UpdateUserScreen.js
+++ app/components/screens/UpdateUserScreen.js
@@ -13,6 +13,11 @@ const UpdateUserScreen = ({checkLoggedIn, navigation, user, fetchingData, update
     const [phoneNumber, setPhoneNumber] = React.useState(user.data.phoneNumber);
     const [photo, setPhoto] = React.useState({});
 
+    //validation
+    const [requiredName, setRequiredName] = React.useState(false);
+    const [requiredPhoneNumber, setRequiredPhoneNumber] = React.useState(false);
+    const [mustPhoneNumber, setMustPhoneNumber] = React.useState(false);
+    
     const checkSourceImage = () =>{
         if(Object.keys(photo).length !== 0)
             return photo.uri;
@@ -44,7 +49,6 @@ const UpdateUserScreen = ({checkLoggedIn, navigation, user, fetchingData, update
         });
     };
 
-
     useEffect(() => {
         checkLoggedIn({ navigation });
         return () => {
@@ -59,12 +63,24 @@ const UpdateUserScreen = ({checkLoggedIn, navigation, user, fetchingData, update
             </View>
         )
     }
+
+    function checkUpdateInformation(requiredName, requiredPhoneNumber, mustPhoneNumber){
+        if(requiredName == false && requiredPhoneNumber == false && mustPhoneNumber == false)
+            return true;
+        return false;
+    }
+
     return (
         <View>
             <View style={{backgroundColor: '#132833', height: 53, flexDirection:'row' ,justifyContent:'space-around', alignItems:'center'}}>
                 <Icon.Button name="close" color='#D9D9D9' backgroundColor='#132833' size={30} onPress={()=>navigation.navigate('User')}></Icon.Button>
                 <Text style={navigationTittle.style}>Chỉnh sửa trang cá nhân</Text>
-                <Icon.Button name='check' color='#06BBD8' backgroundColor='#132833' size={30} onPress={()=>updateUserInformation({name, phoneNumber, photo, navigation})} ></Icon.Button>
+                <Icon.Button name='check' color='#06BBD8' backgroundColor='#132833' size={30}
+                    onPress={() => {
+                        if (checkUpdateInformation(requiredName, requiredPhoneNumber, mustPhoneNumber))
+                            updateUserInformation({ name, phoneNumber, photo, navigation });
+                        else alert('Chỉnh sửa thất bại');
+                    }} ></Icon.Button>
             </View>
             <ScrollView style={{ width: '100%', height: '100%', backgroundColor: '#204051', paddingHorizontal: 30 }}>
                 <View style={{ alignItems: "center", marginBottom: 28 }}>
@@ -74,9 +90,30 @@ const UpdateUserScreen = ({checkLoggedIn, navigation, user, fetchingData, update
                     </TouchableOpacity>
                 </View>
                 <Text style={input.label}>Họ và tên</Text>
-                <TextInput style={input.text} onChangeText={(e)=>{setName(e.valueOf())}}>{name}</TextInput>
+                <TextInput style={input.textWithValidate} onChangeText={(e) => {
+                    setName(e.valueOf())
+                    if (e.valueOf().length == 0)
+                        setRequiredName(true);
+                    else
+                        setRequiredName(false);
+                }}>{name}</TextInput>
+                <Text style={{ color: 'red', marginBottom: 10, fontSize: 13 }}>{requiredName ? <Text>Họ tên không được bỏ trống</Text> : ''}</Text>
                 <Text style={input.label}>Số điện thoại</Text>
-                <TextInput style={input.text} keyboardType="number-pad" onChangeText={(e)=>{setPhoneNumber(e.valueOf())}}>{phoneNumber}</TextInput>
+                <TextInput style={input.textWithValidate} keyboardType="number-pad"
+                    onChangeText={(e) => {
+                        setPhoneNumber(e.valueOf())
+                        if (e.valueOf().length == 0)
+                            setRequiredPhoneNumber(true);
+                        else
+                            setRequiredPhoneNumber(false);
+                        if (/^-?[\d.]+(?:e-?\d+)?$/.test(e.valueOf()) == true)
+                            setMustPhoneNumber(false);
+                        else
+                            setMustPhoneNumber(true);
+                    }}>{phoneNumber}</TextInput>
+                <Text style={{ color: 'red', marginBottom: 10, fontSize: 13 }}>
+                    {(requiredPhoneNumber) ? <Text>Số điện thoại không được bỏ trống</Text> : mustPhoneNumber ? <Text>Số điện thoại chỉ được chứa số</Text> : ''}
+                </Text>
             </ScrollView>
         </View>
     );
